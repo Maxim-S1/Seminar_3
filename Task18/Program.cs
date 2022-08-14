9dab86b75c1a399684776c5bdbd10934deada657
Maxim <sibmaks.khv@mail.ru>
2022-08-14 11:36:01 +1000

    Add Task18

---
+++ Task18/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите прогламму, которая по заданному номеру четверти, показывает диапазон
+// возможных координат тосек в этой четверти (X и Y).
+
+Console.WriteLine("Введите номер координатной четверти ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 1)
+{
+    Console.WriteLine("Четверть 1 соответствует: X > 0 и Y > 0");
+}
+else if (number == 2)
+{
+    Console.WriteLine("Четверть 1 соответствует: X < 0 и Y > 0");
+}
+else if (number == 3)
+{
+    Console.WriteLine("Четверть 1 соответствует: X < 0 и Y < 0");
+}
+else if (number == 4)
+{
+    Console.WriteLine("Четверть 1 соответствует: X > 0 и Y < 0");
+}
+else
+{
+    Console.WriteLine("Такой координатной четверти не существует");
+}
